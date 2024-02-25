2864a85b88c3d5832ff4da6413e07028fb9521a9
graef <simonjn45@gmail.com>
2024-02-25 09:49:41 +0100

    Finished ... only a small dumb problem with connection from multiple sources is still there.

--- README.md
+++ README.md
@@ -6,25 +6,29 @@ A small web server written in Rust.
 
 - [Coding a Web Server in 25 Lines - Computerphile](https://youtu.be/7GBlCinu9yg)
 - [Rust Book: Final Project: Building a Multithreaded Web Server](https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html)
+- [Wikipedia - HTTP](https://en.wikipedia.org/wiki/HTTP)
 
 ## design
 
 Some design goals:
 
-1. minimal + multithreaded
+1. minimal (in code size and dependencies) + resiliant + multithreaded (maybe?)
 2. always return index.html
 3. When no index.html page -> costum file browser
 4. Ignore /../ request -> default to /
-5. When in this address: "address/blog/" blog is not a dictionary (with an index.html file in it), but a file -> return the file
+5. When in this address: "address/blog/" blog is not a dictionary (with an
+   index.html file in it), but a file -> return the file
 6. Only allow http requests ... for now ...
 7. Make it fancy like `serve` ... with Information
-   - print fancy header with local network and localhost addresses
+   - print fancy header localhost address
    - print fancy request
    - print fancy answer
-8. Make the IP address default to the current ip address and make it available to the network
+8. Make the IP address default to the current ip address and make it available
+   to the network
 9. Add some options to maybe change the port, if not defined
-10. If a folder / file is not existing look into base folder and return the defined errorcode.html
-11. If such a file does not exist look if the file exist in an error folde`.html
+10. If a folder / file is not existing look into base folder and return the
+    defined errorcode.html
+11. If such a file does not exist look if the file exist in an error folder.html
 
 ## start server
 
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,87 @@
 use std::io::{BufRead, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+
+/// cut both (first & last) char
+fn cbc(s: String) -> String {
+    if s.len() <= 2 {
+        return s;
+    }
+    s[1..s.len() - 1].to_string()
+}
+
+/// trim slash at start and end .. if it matches
+fn trim(s: String) -> String {
+    s.trim_start_matches("/").trim_end_matches("/").to_owned()
+}
+
+/// thrim slash in start
+fn trim_s(s: String) -> String {
+    s.trim_start_matches("/").to_owned()
+}
+
+fn index_file(path: String) -> bool {
+    file(path + "index.html")
+}
+
+fn file(path: String) -> bool {
+    Path::new(&path).is_file()
+}
+
+fn dir(path: String) -> bool {
+    Path::new(&path).is_dir()
+}
+
+fn read_file(path: String) -> Vec<u8> {
+    std::fs::read(path).unwrap()
+}
+
+fn handle_request(res: String) -> Vec<u8> {
+    let mut ok_message = b"HTTP/1.1 200 OK\r\n\r\n".to_vec();
+    let mut error_message = b"HTTP/1.1 404 NOT FOUND\r\n\r\n".to_vec();
+    if res.ends_with("/") {
+        if index_file(trim_s(res.clone())) {
+            ok_message.append(&mut read_file(trim_s(res.clone()) + "index.html"));
+            return ok_message;
+        }
+        if dir(cbc(res.clone())) {
+            ok_message.append(
+                &mut b"<!DOCTYPE html><html lang=\"en\"><head></head><body><h1>This here should be a fancy file manager</h1></body></html>".to_vec()
+            );
+            return ok_message;
+        }
+    }
+    if file(trim_s(res.clone())) {
+        ok_message.append(&mut read_file(trim_s(res.clone())));
+        return ok_message;
+    }
+    if file(trim(res.clone()) + ".html") {
+        ok_message.append(&mut read_file(trim(res.clone()) + ".html"));
+        return ok_message;
+    }
+    // 404 error
+    error_message.append(
+        &mut b"<!DOCTYPE html><html lang=\"en\"><head></head><body><h1>404 - NOT FOUND</h1></body></html>".to_vec(),
+    );
+    error_message
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut rdr = std::io::BufReader::new(&mut stream);
+    let mut l = String::new();
+    rdr.read_line(&mut l).unwrap();
+    match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
+        ["GET", resource, "HTTP/1.1"] => {
+            stream.write_all(&handle_request(resource.to_string())).unwrap();
+        }
+        _ => {}
+    };
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    for mut stream in listener.incoming().flatten() {
-        let mut rdr = std::io::BufReader::new(&mut stream);
-        let mut l = String::new();
-        rdr.read_line(&mut l).unwrap();
-        match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
-            ["GET", resource, "HTTP/1.1"] => {
-                loop {
-                    let mut l = String::new();
-                    rdr.read_line(&mut l).unwrap();
-                    if l.trim().is_empty() {
-                        break;
-                    }
-                    print!("{l}")
-                }
-                let mut p = std::path::PathBuf::new();
-                p.push(resource.trim_start_matches("/"));
-                if resource.ends_with("/") {
-                    p.push("index.html");
-                }
-                if resource.starts_with("../") {
-                    break;
-                }
-                stream.write_all(b"HTTP/1.1 200\r\n\r\n").unwrap();
-                stream.write_all(&std::fs::read(p).unwrap()).unwrap();
-            }
-            _ => todo!(),
-        };
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream.try_clone().unwrap());
     }
 }
